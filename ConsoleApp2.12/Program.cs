f60ac2d4471c6f3db58178827a1f7a63c21286a5
Alex Zverev <lordzver007@yandex.ru>
2022-04-04 15:03:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2.12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp2._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int halfBracketsCount = 0;
+            int fullBracketsCount = 0;
+            int amountOfRightBrackets = 0;
+            int amountOfLeftBrackets = 0;
+
+            string  text = "(())";   
+            foreach (var symbol in text)
+            {                    
+                Console.Write(symbol);
+                if (symbol == '(')
+                {
+                    amountOfLeftBrackets++;
+                    halfBracketsCount++;                                       
+                }
+                else if (symbol == ')' || halfBracketsCount != 0)
+                {
+                    amountOfRightBrackets++;
+                    halfBracketsCount--;
+                    fullBracketsCount++;                    
+                }                
+            }
+            if (amountOfRightBrackets == amountOfLeftBrackets)
+            {
+                Console.WriteLine("\nМаксимальная глубина скобок: " + fullBracketsCount);                
+            }
+            else if(amountOfLeftBrackets  > amountOfRightBrackets || amountOfRightBrackets > amountOfLeftBrackets )
+            {
+                Console.WriteLine("\nНекорректное количество скобок");
+            }
+            
+        }
+    }
+}
